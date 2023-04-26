28187b859fecb306533287a9e07bd357f1157a2c
clapper <65047073+BodyaKov10@users.noreply.github.com>
2023-04-26 22:37:38 +0300

    Create 26.04.cpp

---
+++ 26.04.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    std::cout << "      /\\      \n";
+    std::cout << "     /  \\     \n";
+    std::cout << "    /    \\    \n";
+    std::cout << "   /------\\   \n";
+    std::cout << "  /|      |\\  \n";
+    std::cout << " /_|______|_\\ \n";
+    std::cout << "|           | \n";
+    std::cout << "|    ___    | \n";
+    std::cout << "|   |___|   | \n";
+    std::cout << "|           | \n";
+    std::cout << "|___________| \n";
+    std::cout << "    ||  ||    \n";
+    std::cout << "    ||  ||    \n";
+    std::cout << "    ||  ||    \n";
+    std::cout << "    ||  ||    \n";
+    return 0;
+}
