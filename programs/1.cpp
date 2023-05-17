2c06af97351784699398dc923bd8130349b879ae
clapper <65047073+BodyaKov10@users.noreply.github.com>
2023-05-17 14:46:12 +0300

    Update 1.cpp

--- programs/1.cpp
+++ programs/1.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    double a, b, c, avg;
-    cout << "Введите три числа: ";
-    cin >> a >> b >> c;
-    avg = (a + b + c) / 3;
-    cout << "Среднее арифметическое: " << avg << endl;
+int main() {
+    double size, speed;
+    cout << "Введите размер фильма в ГБ: ";
+    cin >> size;
+    cout << "Введите скорость интернет-соединения в Мб/с: ";
+    cin >> speed;
+
+    // Конвертируем размер фильма в мегабайты
+    double sizeInMB = size * 1024;
+
+    // Вычисляем время скачивания в секундах
+    double timeInSeconds = sizeInMB / (speed * 8);
+
+    // Преобразуем время в часы, минуты и секунды
+    int hours = timeInSeconds / 3600;
+    int minutes = (int(timeInSeconds) % 3600) / 60;
+    int seconds = int(timeInSeconds) % 60;
+
+    cout << "Время скачивания: " << hours << " часов " << minutes << " минут " << seconds << " секунд." << endl;
+
     return 0;
 }
+
