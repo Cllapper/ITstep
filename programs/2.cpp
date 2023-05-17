f301b369c0c84093c594dec6063d4ed9fd09d67c
clapper <65047073+BodyaKov10@users.noreply.github.com>
2023-05-17 14:46:27 +0300

    Update 2.cpp

--- programs/2.cpp
+++ programs/2.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    double a, b, x;
-    cout << "Введите a и b: ";
-    cin >> a >> b;
-    if (a == 0 && b == 0) {
-        cout << "Уравнение имеет бесконечное множество решений" << endl;
-    } else if (a == 0 && b != 0) {
-        cout << "Уравнение не имеет решений" << endl;
-    } else {
-        x = -b / a;
-        cout << "Корень уравнения " << a << "x + " << b << " = 0: " << x << endl;
-    }
+int main() {
+    int flashDriveSize;
+    cout << "Введите размер флешки в ГБ: ";
+    cin >> flashDriveSize;
+
+    // Конвертируем размер флешки в мегабайты
+    int flashDriveSizeInMB = flashDriveSize * 1024;
+
+    // Вычисляем количество фильмов, помещающихся на флешку
+    int moviesCount = flashDriveSizeInMB / 760;
+
+    cout << "На флешку поместится " << moviesCount << " фильмов." << endl;
+
     return 0;
 }
