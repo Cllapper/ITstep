4af0ee048f8123435f3d6a5d4ea71f7038a11eac
clapper <65047073+BodyaKov10@users.noreply.github.com>
2023-05-23 14:14:15 +0300

    Update 7.cpp

--- programs/7.cpp
+++ programs/7.cpp
@@ -1,17 +1,36 @@
 #include <iostream>
-#include <cmath>
-using namespace std;
 
 int main() {
-    const double PI = 3.14159;
-    double radius;
-    cout << "Введите радиус Земли (в километрах): ";
-    cin >> radius;
+    double length, width, height;
+    double paintPerSquareMeter;
+    double windowAndDoorArea;
+    double paintCostPerLiter;
 
-    // Расчет площади поверхности Земли
-    double surfaceArea = 4 * PI * pow(radius, 2);
+    std::cout << "Введите длину комнаты: ";
+    std::cin >> length;
 
-    cout << "Площадь поверхности Земли: " << surfaceArea << " квадратных километров." << endl;
+    std::cout << "Введите ширину комнаты: ";
+    std::cin >> width;
+
+    std::cout << "Введите высоту комнаты: ";
+    std::cin >> height;
+
+    std::cout << "Введите расход краски на 1м2 стены в литрах: ";
+    std::cin >> paintPerSquareMeter;
+
+    std::cout << "Введите площадь окон и дверей: ";
+    std::cin >> windowAndDoorArea;
+
+    std::cout << "Введите стоимость 1 литра краски в гривнах: ";
+    std::cin >> paintCostPerLiter;
+
+    double wallArea = 2 * (length + width) * height;
+    double totalPaintArea = wallArea - windowAndDoorArea;
+    double paintAmount = totalPaintArea * paintPerSquareMeter;
+    double totalCost = paintAmount * paintCostPerLiter;
+
+    std::cout << "Количество краски: " << paintAmount << " литров" << std::endl;
+    std::cout << "Общая стоимость краски: " << totalCost << " гривен" << std::endl;
 
     return 0;
 }
