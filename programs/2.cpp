c0b88cc0a000db73e4c96eb286ce8a3cbad3cf1f
clapper <65047073+BodyaKov10@users.noreply.github.com>
2023-05-23 14:13:14 +0300

    Update 2.cpp

--- programs/2.cpp
+++ programs/2.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
-using namespace std;
 
 int main() {
-    int flashDriveSize;
-    cout << "Введите размер флешки в ГБ: ";
-    cin >> flashDriveSize;
+    int num;
+    std::cout << "Введите 4-значное число: ";
+    std::cin >> num;
 
-    // Конвертируем размер флешки в мегабайты
-    int flashDriveSizeInMB = flashDriveSize * 1024;
+    // Извлекаем цифры
+    int firstDigit = num / 1000;
+    int secondDigit = (num / 100) % 10;
+    int thirdDigit = (num / 10) % 10;
+    int fourthDigit = num % 10;
 
-    // Вычисляем количество фильмов, помещающихся на флешку
-    int moviesCount = flashDriveSizeInMB / 760;
+    // Выполняем вычисления
+    int sum = firstDigit + thirdDigit;
+    int diff = secondDigit - fourthDigit;
 
-    cout << "На флешку поместится " << moviesCount << " фильмов." << endl;
+    std::cout << "Сумма первой и третьей цифры: " << sum << std::endl;
+    std::cout << "Разность второй и четвертой цифры: " << diff << std::endl;
 
     return 0;
 }
